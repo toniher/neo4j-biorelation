c4eabd530c45ef2b006b93f2113574015abd6989
Toni Hermoso Pulido <toniher@cau.cat>
2018-08-10 18:11:02 +0200

    adding go import

---
+++ scripts/generateGOnodes.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import csv
+import sys
+import pprint
+
+def main(argv):
+
+        if len(sys.argv) < 2:
+                sys.exit()
+
+        termfile = sys.argv[1]
+        termdeffile = sys.argv[2]
+
+        definition_list={}
+
+        reader = csv.reader(open(termdeffile),delimiter="\t")
+        
+        for row in reader:
+            
+            definition = row[1]
+            definition = definition.replace('"', '\\"')
+            definition_list[str(row[0])] = definition
+
+
+        reader = csv.reader(open(termfile),delimiter="\t")
+        
+        print( "id\tname\tterm_type\tacc\tdefinition" )
+
+        
+        for row in reader:
+                print( row[0] + "\t" + row[1] + "\t" + row[2] + "\t" + row[3] + "\t", end='' )
+                if row[0] in  definition_list :
+                        print( definition_list[ row[0] ], end='' )
+                print( "\n", end='' )
+            
+
+
+if __name__ == "__main__":
+        main(sys.argv[1:])
--- scripts/go.sh
+++ scripts/go.sh
@@ -1,7 +1,10 @@
+#!/bin/bash
+
 set -ueo pipefail
 
 # CONFIG parameters
 
+NEO4JSHELL=/data/soft/neo4j-community-3.4.5/bin/cypher-shell
 GOURL=http://archive.geneontology.org/latest-lite/go_weekly-seqdb-tables.tar.gz
 GODIR=/data/db/go/
 MOMENTDIR=/data/toniher
@@ -19,7 +22,32 @@ rm go_weekly-seqdb-tables.tar.gz
 
 cd $SCRIPTPATH
 
-python neo4j2-import-go.py $GODIR/go_weekly-seqdb-tables/term.txt  $GODIR/go_weekly-seqdb-tables/term_definition.txt $GODIR/go_weekly-seqdb-tables/term2term.txt
+# IN PROCESS
+
+echo "CREATE CONSTRAINT ON (n:GO_TERM) ASSERT n.acc IS UNIQUE"
+$NEO4JSHELL "CREATE CONSTRAINT ON (n:GO_TERM) ASSERT n.acc IS UNIQUE"
+echo "CREATE CONSTRAINT ON (n:GO_TERM) ASSERT n.id IS UNIQUE"
+$NEO4JSHELL "CREATE CONSTRAINT ON (n:GO_TERM) ASSERT n.id IS UNIQUE"
+
+
+# merge term.txt with term_definition in below
+GONODES=$MOMENTDIR/gonodes.csv
+GORELS=$MOMENTDIR/gorels.csv
+
+
+python generateGOnodes.py $GODIR/go_weekly-seqdb-tables/term.txt $GODIR/go_weekly-seqdb-tables/term_definition.txt > $GONODES
+
+echo "LOAD CSV WITH HEADERS FROM \"file://${GONODES}\" AS row FIELDTERMINATOR \"\t\" CREATE (n:GO_TERM) SET n = row ;"
+$NEO4JSHELL "LOAD CSV WITH HEADERS FROM \"file://${GONODES}\" AS row FIELDTERMINATOR \"\t\" CREATE (n:GO_TERM) SET n = row ;" >> $MOMENTDIR/syn.out 2>> $MOMENTDIR/syn.err
+
+# replace term2term.txt to a version with 3 column and with rel replaced with its name version
+python generateGOrels.py $GODIR/go_weekly-seqdb-tables/term.txt $GODIR/go_weekly-seqdb-tables/term2term.txt > $GORELS
+
+echo "LOAD CSV WITH HEADERS FROM \"file://${GORELS}\" AS row FIELDTERMINATOR \"\t\" MATCH (c:GO_TERM {id:row.target}), (p:GO_TERM {id:row.source}) CREATE (c)-[:row.rel]->(p) ;"
+
+$NEO4JSHELL "LOAD CSV WITH HEADERS FROM \"file://${$GORELS}\" AS row FIELDTERMINATOR \"\t\" MATCH (c:GO_TERM { id:row.target} ), (p:GO_TERM { id:row.source } ) CREATE (c)-[:row.rel]->(p) ;" >> $MOMENTDIR/syn.out 2>> $MOMENTDIR/syn.err
+
+
 
 
 
