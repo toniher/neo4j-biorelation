221df24411e378958ab33eabe44a64ace4961ad4
Toni Hermoso Pulido <toniher@cau.cat>
2019-08-09 12:14:36 +0200

    adding intact·

--- populate/filterIntactReduced.py
+++ populate/filterIntactReduced.py
@@ -3,6 +3,7 @@ from __future__ import print_function
 import csv
 import sys
 import pprint
+import re
 
 def main(argv):
 
@@ -15,14 +16,31 @@ def main(argv):
     
     for row in reader:
         
+        amol = row[0]
+        bmol = row[1]
+        method = ""
+        intype = ""
+        confidence = ""
+        update = row[5]
+        
         # Interaction detection method(s)
-        print( row[2] )
+        match1 = re.findall( "psi-mi:\"(\S+)\"", row[2] )
+        if len( match1 ) > 0 :
+            method = match1[0]
 
         # Interaction type
-        
+        match2 = re.findall( "psi-mi:\"(\S+)\"", row[3] )
+        if len( match2 ) > 0 :
+            intype = match2[0]
+            
         # Confidence
+        match3 = re.findall( "intact\-miscore\:(\S+)", row[4] )
+        if len( match3 ) > 0 :
+            confidence = match3[0]
         
-        # Update date
+        if amol and bmol and update :
+        
+            print( amol + "\t" + bmol + "\t" + method + "\t" + intype + "\t" + confidence + "\t" + update )
 
 
 if __name__ == "__main__":
